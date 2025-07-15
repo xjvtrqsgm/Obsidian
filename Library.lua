5c73d302c047b72e68c8606a8196a96390e86ea2
upio <75510171+notpoiu@users.noreply.github.com>
2025-07-14 20:22:03 -0400

    fix: pushed ColorPicker fix provided in issue #40

--- Library.lua
+++ Library.lua
@@ -2328,13 +2328,13 @@ do
 
             ColorPicker.Transparency = Info.Transparency and Transparency or 0
             ColorPicker:SetHSVFromRGB(Color)
-            ColorPicker:Display()
+            ColorPicker:Update()
         end
 
         function ColorPicker:SetValueRGB(Color, Transparency)
             ColorPicker.Transparency = Info.Transparency and Transparency or 0
             ColorPicker:SetHSVFromRGB(Color)
-            ColorPicker:Display()
+            ColorPicker:Update()
         end
 
         Holder.MouseButton1Click:Connect(ColorMenu.Toggle)
